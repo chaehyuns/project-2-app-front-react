3861cae62a288e339944392975f59c3a1cb844e7
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2022-07-10 18:17:17 +0900

    Implement login layout

---
+++ src/pages/Delivery.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Delivery() {
+  return (
+    <View>
+      <Text>배달</Text>
+    </View>
+  );
+}
+
+
+export default Delivery;
